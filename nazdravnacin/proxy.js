60828147c4c5451cfe03cf3785fe4b900ef940ab
apalac13 <anabradaric77@gmail.com>
2026-07-02 17:50:09 +0200

    changed proxy

--- nazdravnacin/proxy.js
+++ nazdravnacin/proxy.js
@@ -1,7 +1,9 @@
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./src/i18n/routing.js";
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  locales: ["hr", "en"],
+  defaultLocale: "hr",
+});
 
 export const config = {
   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
