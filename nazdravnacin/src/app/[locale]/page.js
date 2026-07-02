b5b62a40c1a4f18091648a038a89d1f2c017235e
apalac13 <anabradaric77@gmail.com>
2026-07-02 17:43:05 +0200

    returned page.js

--- nazdravnacin/src/app/[locale]/page.js
+++ nazdravnacin/src/app/[locale]/page.js
@@ -1,3 +1,25 @@
+"use client";
+
+import Pocetna from "@/components/Pocetna";
+import Kartice from "@/components/Kartice";
+import Onama from "@/components/Onama";
+import Paketi from "@/components/Paketi";
+import TjedniZdravlja from "@/components/TjedniZdravlja";
+import Galerija from "@/components/Galerija";
+import PoklonBonovi from "@/components/PoklonBonovi";
+import Newsletter from "@/components/Newsletter";
+
 export default function Home() {
-  return <h1>Radi!</h1>;
+  return (
+    <>
+      <Pocetna />
+      <Kartice />
+      <Onama />
+      <Paketi />
+      <TjedniZdravlja />
+      <Galerija />
+      <PoklonBonovi />
+      <Newsletter />
+    </>
+  );
 }
