51eb5481da19e9f16af3eeecfc76decba7166fa0
apalac13 <anabradaric77@gmail.com>
2026-07-02 16:18:55 +0200

    added  generateStaticParams

--- nazdravnacin/src/app/[locale]/layout.js
+++ nazdravnacin/src/app/[locale]/layout.js
@@ -2,6 +2,10 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import PageWrapper from "@/components/PageWrapper";
 
+export function generateStaticParams() {
+  return [{ locale: "hr" }, { locale: "en" }];
+}
+
 export default async function LocaleLayout({ children, params }) {
   const { locale } = await params;
   const messages = await getMessages();
